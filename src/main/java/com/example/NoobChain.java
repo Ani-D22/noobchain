03ad8760d21debb1b7923a1a3b6938af294f042a
Aniket Date <dateaniket07@gmail.com>
2025-07-23 19:18:11 +0530

    added support for calculating previousHash; changed difficulty to 5;

--- src/main/java/com/example/NoobChain.java
+++ src/main/java/com/example/NoobChain.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class NoobChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
-    public static int difficulty = 4;
+    public static int difficulty = 5;
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
 
